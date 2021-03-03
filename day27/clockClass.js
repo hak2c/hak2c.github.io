9c235c67a71b8478e6a9c04ce946329deddb9436
hapt <hak2cqn@gmail.com>
2021-03-03 17:31:44 +0700

    update

--- day27/clockClass.js
+++ day27/clockClass.js
@@ -1,87 +1,107 @@
 export default class Clock {
-  constructor(selector, lapSelector, startButton, stopButton, resetButton) {
-    this.el = document.querySelector(selector);
-    this.lap = document.querySelector(lapSelector);
-    this.startButton = document.querySelector(startButton);
-    this.stopButton = document.querySelector(stopButton);
-    this.resetButton = document.querySelector(resetButton);
-    this.hour = 0;
+  constructor(clock, record, start, stop, reset, records) {
+    this.clock = clock;
+    this.recordBtn = record;
+    this.startBtn = start;
+    this.stopBtn = stop;
+    this.resetBtn = reset;
+    this.records = records;
+
+    this.running = false;
+
     this.minute = 0;
     this.second = 0;
     this.milisecond = 0;
     this.count = 0;
+
+    this.init();
+  }
+  init() {
+    this.mNode = document.createTextNode("00");
+    this.sNode = document.createTextNode("00");
+    this.miliNode = document.createTextNode("00");
+
+    this.startBtn.onclick = () => this.start();
+    this.recordBtn.onclick = () => this.record();
+    this.stopBtn.onclick = () => this.stop();
+    this.resetBtn.onclick = () => this.reset();
+
+    this.active();
     this.render();
   }
+  active() {
+    if (this.running) {
+      this.startBtn.disabled = true;
+      this.recordBtn.disabled = false;
+      this.stopBtn.disabled = false;
+      this.resetBtn.disabled = false;
+    } else {
+      this.startBtn.disabled = false;
+      this.recordBtn.disabled = true;
+      this.stopBtn.disabled = true;
+      this.resetBtn.disabled = true;
+    }
+  }
   render() {
-    this.el.innerHTML = `<span id="hour">${String(this.hour).padStart(
-      2,
-      "0"
-    )}</span>:<span id="minute">${String(this.minute).padStart(
-      2,
-      "0"
-    )}</span>:<span id="hour">${String(this.second).padStart(
-      2,
-      "0"
-    )}</span>&nbsp;<span id="milisecond">${String(this.milisecond).padStart(
-      2,
-      "0"
-    )}</span>`;
+    this.clock.appendChild(this.mNode);
+    this.clock.append(":");
+    this.clock.appendChild(this.sNode);
+    this.clock.append(":");
+    this.clock.appendChild(this.miliNode);
   }
 
   start() {
-    this.startButton.setAttribute("disabled", true);
-    this.stopButton.removeAttribute("disabled");
-    this.resetButton.setAttribute("disabled", true);
-    this.timer = setInterval(() => {
-      this.checkTime();
-      this.render();
-    }, 10);
+    if (!this.running) {
+      this.timer = setInterval(() => {
+        this.milisecond += 1;
+        if (this.milisecond >= 100) {
+          this.milisecond = 0;
+          this.second++;
+          if (this.second >= 60) {
+            this.second = 0;
+            this.minute++;
+            this.mNode.textContent = String(this.minute).padStart(2, "0");
+          }
+          this.sNode.textContent = String(this.second).padStart(2, "0");
+        }
+        this.miliNode.textContent = String(this.milisecond).padStart(2, "0");
+      }, 10);
+      this.running = true;
+      this.active();
+    }
+  }
+
+  record() {
+    if (this.running) {
+      this.records.innerHTML += `<p class="record">#${++this.count} ${
+        this.clock.textContent
+      }</p>`;
+    }
   }
 
   stop() {
-    this.count++;
-    this.lap.innerHTML += `<p>Lap ${this.count}: ${String(this.hour).padStart(
-      2,
-      "0"
-    )}:${String(this.minute).padStart(2, "0")}:${String(this.second).padStart(
-      2,
-      "0"
-    )}:${String(this.milisecond).padStart(2, "0")}</p>`;
-    this.startButton.removeAttribute("disabled");
-    this.stopButton.setAttribute("disabled", true);
-    this.resetButton.removeAttribute("disabled");
-    clearInterval(this.timer);
+    if (this.running) {
+      clearInterval(this.timer);
+      this.startBtn.disabled = false;
+      this.running = false;
+    }
   }
 
   reset() {
     this.timer && clearInterval(this.timer);
-    this.hour = 0;
     this.minute = 0;
     this.second = 0;
     this.milisecond = 0;
-    this.lap.textContent = "";
     this.count = 0;
-    this.startButton.removeAttribute("disabled");
-    this.stopButton.setAttribute("disabled", true);
-    this.resetButton.removeAttribute("disabled");
-    this.render();
-  }
+    this.running = false;
+    this.records.innerHTML = "";
 
-  checkTime() {
-    if (this.milisecond == 99) {
-      this.milisecond = 0;
-      if (this.second == 59) {
-        this.second = 0;
-        if (this.minute == 59) {
-          this.minute = 0;
-        } else {
-          this.minute++;
-        }
-      } else {
-        this.second++;
-      }
-    } else {
-      this.milisecond++;
-    }
+    this.mNode = document.createTextNode("00");
+    this.sNode = document.createTextNode("00");
+    this.miliNode = document.createTextNode("00");
+    this.clock.innerHTML = "";
+
+    this.active();
+    this.render();
   }
 }
--- day27/index.js
+++ day27/index.js
@@ -1,6 +1,21 @@
 import Clock from "./clockClass.js";
 
-let clock = new Clock(".clock", ".lap-list", "#buttonStart", "#buttonStop", "#buttonReset");
+let clock = document.getElementById("clock");
+let record = document.getElementById("buttonRecord");
+let start = document.getElementById("buttonStart");
+let stop = document.getElementById("buttonStop");
+let reset = document.getElementById("buttonReset");
+let records = document.getElementById("records");
+
+new Clock(clock, record, start, stop, reset, records);
+
+/*let clock = new Clock(
+  ".clock",
+  ".record-list",
+  "#buttonStart",
+  "#buttonStop",
+  "#buttonReset"
+);
 
 document.getElementById("buttonStart").addEventListener("click", function (e) {
   e.preventDefault();
@@ -14,3 +29,4 @@ document.getElementById("buttonReset").addEventListener("click", function (e) {
   e.preventDefault();
   clock.reset();
 });
+*/
